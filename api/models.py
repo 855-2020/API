1caf8f4732f2c04aac2f21975d9f5ef634d70e05
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2020-11-17 12:25:11 -0300

    Add a couple more DB tables

--- api/models.py
+++ api/models.py
@@ -32,14 +32,36 @@ user_roles = Table('user_roles', Base.metadata,
                    )
 
 
+class Model(Base):
+    __tablename__ = "models"
+
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+
+    sectors = relationship("Sector", backref="model")
+    roles = relationship('Role', lazy='dynamic', secondary=lambda: model_roles)
+
+
+model_roles = Table('model_roles', Base.metadata,
+                    Column('model_id', Integer, ForeignKey(Model.id), primary_key=True),
+                    Column('role_id', Integer, ForeignKey(Role.id), primary_key=True),
+                    )
+
+
 class Sector(Base):
     __tablename__ = "sectors"
 
     id = Column(Integer, autoincrement=True, primary_key=True, index=True)
     name = Column(String(100), index=True, nullable=False)
+    model_id = Column(Integer, ForeignKey(Model.id))
+    value_added = Column(Float, nullable=False)
 
-    coefs = relationship("LeontiefCoefficient", lazy="dynamic", foreign_keys=lambda: LeontiefCoefficient.source_id,
-                         back_populates="source")
+    economic_coefs = relationship("EconomicCoefficient", lazy="dynamic",
+                                  foreign_keys=lambda: EconomicCoefficient.source_id,
+                                  back_populates="source")
+    leontief_coefs = relationship("LeontiefCoefficient", lazy="dynamic",
+                                  foreign_keys=lambda: LeontiefCoefficient.source_id,
+                                  back_populates="source")
 
 
 class Activity(Base):
@@ -48,6 +70,18 @@ class Activity(Base):
     id = Column(Integer, autoincrement=True, primary_key=True, index=True)
     name = Column(String(10))
     desc = Column(String(100))
+
+
+class EconomicCoefficient(Base):
+    __tablename__ = "coefs_economic"
+
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    source_id = Column(Integer, ForeignKey("sectors.id"), nullable=False, index=True)
+    target_id = Column(Integer, ForeignKey("sectors.id"), nullable=False, index=True)
+    value = Column(Float, nullable=False)
+
+    source = relationship("Sector", foreign_keys=[source_id])
+    target = relationship("Sector", foreign_keys=[target_id])
 
 
 class LeontiefCoefficient(Base):
