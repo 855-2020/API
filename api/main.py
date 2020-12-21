581665702354ee7c7c4d690602dfb5fd921bfcee
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2020-12-21 19:03:35 -0300

    Remove broken endpoints

--- api/main.py
+++ api/main.py
@@ -45,41 +45,6 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.post("/activity")
-def create_activity(activity: Activity):
-    print(activity)
-
-    return "Activity created!"
-
-
-@app.get("/activities/list", response_model=List[Activity])
-def list_activities(db: Session = Depends(get_db)):
-    return db.query(models.Activity).all()
-
-
-# TODO proper model
-@app.post("/impact", response_model=Dict[int, float])
-def get_impact(values: Dict[int, float], db: Session = Depends((get_db))):
-    coefs = db.query(models.LeontiefCoefficient).filter(
-        models.LeontiefCoefficient.source_id.in_(values.keys())
-    ).all()
-    response: Dict[int, float] = dict()
-    for coef in coefs:
-        if coef.target_id not in response:
-            response[coef.target_id] = 0
-        response[coef.target_id] += coef.value * values[coef.source_id]
-    return response
-
-
-@app.post("/activity/{sector_id}/{sector_value}", response_model=Activity)
-def get_results(sector_id: int, sector_value: float, db: Session = Depends(get_db)):
-    coefs = db.query(models.ActivityCoefficient).filter(
-        models.ActivityCoefficient.sector_id == sector_id
-    ).all()
-    props = {coef.activity.name: sector_value * coef.value for coef in coefs}
-    return Activity(**props)
-
-
 app.include_router(user.router, prefix='/users')
 app.include_router(sector.router, prefix='/sectors')
 app.include_router(model.router, prefix='/models')
